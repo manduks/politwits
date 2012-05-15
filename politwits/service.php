02e7409e54113571ed35245c470938f0e7cc451f
Miguel Salas <miguelsalasmx@localhost.localdomain>
2012-05-14 20:05:11 -0500

    Cambios al back

--- politwits/service.php
+++ politwits/service.php
@@ -8,29 +8,67 @@
 
     include("config.php");
     include("util.php");
+
     class Service{
+
         function __construct(){
+            $this->monitorProcess();
+        }
+
+        //Detiene todos los procesos corriendo que esten monitoreando los tracks
+        function killProcess(){
+            exec("kill $(ps x | grep 'php politwits.php' | cut -f1 -d' ')");
+            exec("kill $(ps x | grep 'php monitor.php' | cut -f1 -d' ')");
+        }
+
+        //Levanta los procesos requeridos
+        function start(){
+
+            $util=new Util();
+
+            $util->sendMail("miguel@codetlan.com",'System Start','Starting...');
+
+            $this->killProcess();
+
+            exec("php politwits.php --track=@EPN,@JosefinaVM,@G_quadri,@lopezobrador_");
+
+
+
+        }
+
+        function restart(){
+
+            $util=new Util();
+
+            $util->sendMail("miguel@codetlan.com",'System Down','Restarting...');
+
+            $this->killProcess();
+
+            exec("php politwits.php --track=@EPN,@JosefinaVM,@G_quadri,@lopezobrador_");
+
+        }
+
+        function monitorProcess(){
+
+            $this->start();
+
             while(true){
                 $proc=NULL;
                 sleep(2);
                 exec("ps x | grep 'php politwits.php'",$proc);
-                if(count($proc)==2){
-                    $util=new Util();
+                if(count($proc)==2)
+                    $this->restart();
 
-                    $util->sendMail("miguel@codetlan.com",'ME CAI','LEVANTEME PENDEJO');
-                    $util->sendMail("rodrigo@codetlan.com",'ME CAI','LEVANTEME PENDEJO');
-                    $util->sendMail("enrique@codetlan.com",'ME CAI','LEVANTEME PENDEJO');
-                    $util->sendMail("eduardo@codetlan.com",'ME CAI','LEVANTEME PENDEJO');
-                    $util->sendMail("armando@codetlan.com",'ME CAI','LEVANTEME PENDEJO');
-                    $util->sendMail("oswaldo@codetlan.com",'ME CAI','LEVANTEME PENDEJO');
+                $proc=NULL;
+                sleep(2);
+                exec("ps x | grep 'php monitor.php'",$proc);
+                if(count($proc)==2)
+                    $this->restart();
 
-                    exec("kill $(ps x | grep 'php monitor.php' | cut -f1 -d' ')");
-
-                    exit;
-
-                }
             }
         }
+
+
     }
     $serv=new Service();
 
