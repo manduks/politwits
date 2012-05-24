44154b9ed9493303debc43c3077d1bda895baaf5
Miguel Salas <miguelsalasmx@localhost.localdomain>
2012-05-24 16:36:02 -0500

    Correccion de bugs de tops y se modifico el frame

--- frame.php
+++ frame.php
@@ -21,13 +21,25 @@
     <!--link rel="stylesheet" href="http://cdn.sencha.io/ext-4.1.0-gpl/resources/css/ext-all.css" /-->
     <link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css" />
     <link rel="stylesheet" href="assets/css/codetlan.css" type="text/css" />
-    <style>
-        body{
-            background-color: #000000;
-        }
-    </style>
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-31605697-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
 </head>
 <body>
+    <script type="text/javascript">
+        document.body.style.backgroundColor=parent.document.getElementById("analitweets").style.backgroundColor;
+        setTimeout("location.reload(true);",20000);
+    </script>
     <a href="http://www.analitweets.com" target="_blank" style="text-decoration: none;">
     <div>
         <div class="candidato" style="float:left;">
@@ -108,5 +120,6 @@
     </div>
     <div style="clear:both; margin-left: 10px;">Powered by @codetlan</div>
     </a>
+
 </body>
 </html>
