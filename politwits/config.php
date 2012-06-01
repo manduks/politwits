e676c4696482195e38c70b45781440750577567a
Rodrigo Loyola <rodrigo@extjs.mx>
2012-05-31 22:35:18 -0500

    cambios

--- politwits/config.php
+++ politwits/config.php
@@ -9,13 +9,19 @@
     date_default_timezone_set("America/Mexico_City");
 
     //Datos de para la conexión de la base de datos
+    //define("USER","root");
+    //define("PASS","r00tdeadman");
+    //define("SERVER","localhost");
+    //define("DATABASE","analitweets");
+    //define("LIMIT",50);
+
     define("USER","codetlan_analitw");
     define("PASS","(t}=EnR(I=E)");
     define("SERVER","174.142.75.228");
     define("DATABASE","codetlan_analitweets");
-	define("LIMIT",50);
+    define("LIMIT",50);
 
-    //Cuentas válidas de twitter
+    //Cuentas válidas de twitter desde las que se realizaran los monitoreos
     $accounts= array(
         "ACCOUNT1" => array("USER" => "Politwitsmx1","PASS" => "codetlanpolitwitsmx.1-", "ACTIVE" => 0),
         "ACCOUNT2" => array("USER" => "Politwitsmx2","PASS" => "codetlanpolitwitsmx.2-", "ACTIVE" => 0),
@@ -30,4 +36,20 @@
     //Cuenta valida de gmail para enviar correos de avisos
     define("EMAIL","politwitsmx@gmail.com");
     define("EMAILPASS","mQ9tB2MfTUsKPe");
+
+    //Cuenta de correo a la que desean que les lleguen los avisos
+    define("EMAIL_INFO","miguel@codetlan.com");
+
+    //Los tracks que desean monitorear
+    $tracks=array(
+        "@EPN",
+        "@JosefinaVM",
+        "@lopezobrador_",
+        "@G_quadri"
+    );
+
+    //La ruta absoluta donde se encuentran los scripts
+    //define("PATH","/home/miguelsalasmx/public_html/politwits/politwits/");
+    //define("PATH","/home/codetlan/system/");
+
 ?>
