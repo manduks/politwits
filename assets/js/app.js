71ce909f4c6ce30bced576e12b0fe98fe2a2f550
Rodrigo Loyola <rodrigo@extjs.mx>
2012-06-01 20:37:08 -0500

    se modifico la posicion del logo de codetlan

--- assets/js/app.js
+++ assets/js/app.js
@@ -601,6 +601,7 @@ Ext.onReady(function(){
 			baseCls:'navbar navbar-fixed-top',
 			items:[
 				'<div class="brand"><a href="https://twitter.com/#!/analitweets" target="_blank">@Analitweets</a></div>',
+				'<a href="http://www.codetlan.com" target="_blank"><img src="assets/img/logo.png" alt="" /></a>','-',
 				{
 					text:'<i class="icon-comment icon-white"></i> <span style="color:#FFFFFF;">Tweets</span>',
                     id:"tweets",
